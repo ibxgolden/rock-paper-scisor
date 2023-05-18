19af09ea0312a6555a1db20e6b3f6fb7b5c9a976
ibxgolden <129530843+ibxgolden@users.noreply.github.com>
2023-05-18 09:23:50 +0300

    Update abc.js
     to add the tasks we neeed  a space to save them so lets create lists that holds our task value

--- abc.js
+++ abc.js
@@ -2,3 +2,11 @@ var listElement = document.querySelector("#app ul");
 var inputElement = document.querySelector("#app input");
 var buttonElement = document.querySelector("#app button");
 var todos = JSON.parse(localStorage.getItem("list_todos")) || [];
+function renderTodos() {
+listElement.innerHTML = "";
+for (todo of todos) {
+var todoElement = document.createElement("li");
+var todoText = document.createTextNode(todo);
+var linkElement = document.createElement("a");
+linkElement.setAttribute("href", "#");
+var pos = todos.indexOf(todo);
